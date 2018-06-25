8339e00d98612f04a881e0b87f7195770b2366c7
Thomas Deruel <thomas.deruel@hetic.net>
2018-06-25 22:22:20 +0200

    usersType modified

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -6,26 +6,37 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prenom')
-            ->add('nom')
-            ->add('email')
-            ->add('password')
-            ->add('sexe')
-            ->add('origine')
-            ->add('actuel')
-        ;
+            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('sexe', TextType::class)
+            ->add('origin', TextType::class)
+            ->add('actuel', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('matricule', HiddenType::class, array(
+                'data' => mt_rand(),
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
+        $resolver->setDefaults(array(
             'data_class' => Users::class,
-        ]);
+        ));
     }
 }
